c18d11c81e70800542b7addba10fe774cc2be53e
yinghui tan <yinghuitan18@gmail.com>
2019-04-05 13:06:44 +0800

    get root routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'cocktails#index'
   resources :doses, only: [:destroy, :index]
 
-  resources :cocktails do
+  resources :cocktails, except: :index do
     resources :doses, only: [:show, :new, :create]
     resources :ingredients, only: [:show, :new, :create]
   end
